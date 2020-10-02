1a5663397d302f031e6bc7aa0a458ba96765b6ec
余越 <1643863272@qq.com>
2020-10-02 17:50:58 +0800

    Display()

--- The Sequence List/main.cpp
+++ The Sequence List/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <vector>
+#include "SequenceList.h"
 using namespace std;
 
 int main() {
 
-	
+	int i = 0;
+	SequenceList<int> a;
+	a.PushBack(12);
+	a.PushBack(123);
+	a.PushBack(1215);
+	a.Display();
+	a.Insert(4, 6);
+	a.Insert(2, 230);
+	a.Insert(4, 6);
+	a.Display();
+	cout << a.Search(6) << endl;
+	cout << a.Search(120) << endl;
 	return 0;
 }
